f8db08b1a2a048ddaded6532796b144df9533239
Cherie <cherietsukayama@gmail.com>
2020-06-01 00:30:13 +0000

    used a function compare numbers in the sort method parameteres.

--- make-array-consecutive-2/index.js
+++ make-array-consecutive-2/index.js
@@ -1,13 +1,17 @@
 function makeArrayConsecutive2(statues) {
-  const sortedArray = statues.sort()
+  const sortedArray = statues.sort((a, b) => a - b)
   let counter = 0;
   for (var i = 0; i < sortedArray.length; i++) {
     let currentItem = sortedArray[i];
     let nextNumber = sortedArray[i + 1];
     if (nextNumber === undefined) {
       return counter;
+    } else if ((currentItem + 1) === nextNumber) {
+      counter + 0;
+      console.log(sortedArray)
+    } else {
+      counter = counter + (nextNumber - currentItem) - 1;
     }
-    counter = counter + (nextNumber - currentItem) - 1;
   }
   return counter;
-}
+}   
